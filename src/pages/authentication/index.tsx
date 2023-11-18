23ed41750a37ea93d5803c829e7561e97fefc187
Uzgur <uzbmv2@gmail.com>
2023-11-18 18:19:41 +0500

    auth page changed

--- src/pages/authentication/index.tsx
+++ src/pages/authentication/index.tsx
@@ -3,10 +3,10 @@ import { NavLink, Outlet } from "react-router-dom";
 export default function Auth() {
   return (
     <div>
-      <main>
-        <NavLink to="/player1">Register 1st Player</NavLink>
+      <main className="auth-container">
+        <NavLink className="auth-btn" to="/player1">Register 1st Player</NavLink>
         <br />
-        <NavLink to="/player2">Register 2nd Player</NavLink>
+        <NavLink className="auth-btn" to="/player2">Register 2nd Player</NavLink>
       </main>
     </div>
   );
